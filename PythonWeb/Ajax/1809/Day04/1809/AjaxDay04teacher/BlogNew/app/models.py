5eaddb2ef7d26e88b14096e0b08f257bec9e9fe5
cooper <huafengdongji@hotmail.com>
2019-01-10 10:02:45 +0800

    果园项目集成

--- PythonWeb/Ajax/1809/Day04/1809/AjaxDay04teacher/BlogNew/app/models.py
+++
@@ -1,4 +0,0 @@
-#根据数据库编写所有的实体类
-#导入 db 到models.py
-from . import db
-#通过db创建实体类
--- PythonWeb/Project/BlogNew/app/models.py
+++ PythonWeb/Project/BlogNew/app/models.py
@@ -1,8 +1,9 @@
-# 编写所有的实体类
+"""编写所有的实体类"""
 from . import db
 
 
 class Blogtype(db.Model):
+    """博客类型"""
     __tablename__ = 'blogtype'
     id = db.Column(db.Integer, primary_key=True)
     type_name = db.Column(db.String(20), nullable=False)
@@ -12,7 +13,9 @@ class Blogtype(db.Model):
         lazy = 'dynamic'
     )
 
+
 class Category(db.Model):
+    """博客种类"""
     __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
     cate_name = db.Column(db.String(50), nullable=False)
@@ -22,7 +25,9 @@ class Category(db.Model):
         lazy = 'dynamic'
     )
 
+
 class Reply(db.Model):
+    """回复"""
     __tablename__ = 'reply'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.ID'), nullable=True)
@@ -32,11 +37,12 @@ class Reply(db.Model):
 
 
 class Topic(db.Model):
+    """帖子"""
     __tablename__ = 'topic'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     pub_date = db.Column(db.DateTime, nullable=False)
-    read_num = db.Column(db.Integer, nullable=True)
+    read_num = db.Column(db.Integer, default=0, nullable=True)
     content = db.Column(db.Text, nullable=False)
     images = db.Column(db.Text, nullable=True)
     blogtype_id = db.Column(db.Integer, db.ForeignKey('blogtype.id'), nullable=True)
@@ -55,6 +61,7 @@ class Topic(db.Model):
 
 
 class User(db.Model):
+    """用户"""
     __tablename__ = 'user'
     ID = db.Column(db.Integer, primary_key=True)
     loginname = db.Column(db.String(50), nullable=False)
@@ -62,7 +69,7 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False)
     url = db.Column(db.String(50), nullable=True)
     upwd = db.Column(db.String(50), nullable=False)
-    is_author = db.Column(db.Boolean(4), default=False)
+    is_author = db.Column(db.Boolean, default=False)
     replys = db.relationship(
         'Reply',
         backref='user',
@@ -81,6 +88,7 @@ class User(db.Model):
 
 
 class Voke(db.Model):
+    """点赞"""
     __tablename__ = 'voke'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.ID'), nullable=False)
